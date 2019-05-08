d82ff1dcf76e39c3ab4623e7773784cb77bb19f7
unknown <daliborpetric99@gmail.com>
2019-05-08 16:28:40 +0200

    Updates

---
+++ precache-manifest.ba99dd4020526cdc65336aabdfc921d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5776250977928ab448c47f832ff2ff72",
+    "url": "/react-trivia/index.html"
+  },
+  {
+    "revision": "5160f520309537769cac",
+    "url": "/react-trivia/static/css/main.d39839a7.chunk.css"
+  },
+  {
+    "revision": "10aa3c6772ca85f6e2ad",
+    "url": "/react-trivia/static/js/2.dc6fc661.chunk.js"
+  },
+  {
+    "revision": "5160f520309537769cac",
+    "url": "/react-trivia/static/js/main.f3097f77.chunk.js"
+  },
+  {
+    "revision": "d7d639b6d6fb5a48ad56",
+    "url": "/react-trivia/static/js/runtime~main.46ce7e89.js"
+  }
+]);
